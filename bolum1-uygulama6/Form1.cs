a1e9d011ba3928bfb9aa1b22b0af4b7e6b7750e7
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-09-25 15:46:44 +0300

    Add files via upload

---
+++ bolum1-uygulama6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum1_uygulama6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = 10;
+            int sayi2 = 50;
+
+            int toplam;
+            toplam = sayi1 + sayi2;
+            MessageBox.Show("50" + toplam.ToString());
+        }
+    }
+}
